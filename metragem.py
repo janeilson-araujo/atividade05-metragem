2e57902936ae244dff5f6621fa4678d681401d6f
janeilson-araujo <janeilsonpaulino013@gmail.com>
2023-10-11 13:00:05 -0300

    atividade 05 metragem python

---
+++ metragem.py
@@ -0,0 +1,7 @@
+# Digite uma metragem e veja isso em cm e mm:
+
+print("conversão de metros para centimetros e milimetros")
+metros = float(input("digite o valor em metros para ser convertido:"))
+centimetros = metros * 100
+milimetro = centimetros * 10
+print(f"       metros:{metros} \n  centimetros:{centimetros}  \n   milimetros:{milimetro}")
